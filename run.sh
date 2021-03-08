81411c9bc17fdd06ec56ba05af63e012d450effd
aditidoc <79784930+aditidoc@users.noreply.github.com>
2021-03-08 08:25:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
 
-CONF="/root/opencanary.conf"
+CONF="/home/ferraglia-ftp/opencanary.conf"
 TEMP_CONF="/root/.opencanary.conf"
 
+
+defaultconf=$(python -c "from __future__ import print_function; from pkg_resources import resource_filename; print(resource_filename('opencanary', 'data/settings.json'))")
+# il pvc con la configurazione Ã¨ sotto /home/ferraglia-ftp, qui ci devo anche confiurare i logs. 
+# se il file esiste lo copio sotto la directory di lavoro e faccio partire il processo
+# altrimenti lo creo da quello di default e lo copio, eventualente andrÃ  editato
 if [ -f $CONF ]; then
 	echo "INFO: Main configuration file found"
-	opencanaryd --dev
-elif [ -f $TEMP_CONF ]; then
-	echo "INFO: Temp configuration file found"
-	opencanaryd --dev
+        mkdir /etc/opencanaryd
+        cp "${CONF}" /etc/opencanaryd/opencanary.conf
+        twistd -y /usr/local/bin/opencanary.tac
 else
-	opencanaryd --copyconfig && echo "A Config file was generated at /root/.opencanary.conf. If the volume is mapped, you have a copy and should move it to /root/opencanary.conf" && echo "In either case, you can rerun this container and have it run."
+        mkdir /etc/opencanaryd
+	cp "${defaultconf}" /etc/opencanaryd/opencanary.conf
+        cp "${defaultconf}" "${CONF}"
+        twistd -y /usr/local/bin/opencanary.tac
 fi
+
 
